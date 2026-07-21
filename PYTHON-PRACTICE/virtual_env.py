dd8f7c6e0077f484ed5af8d0799a7ad029a1e925
Arjit Verma <arjitverma111@gmail.com>
2026-07-21 20:17:16 +0530

    Delete PYTHON-PRACTICE directory

--- PYTHON-PRACTICE/virtual_env.py
+++
@@ -1,5 +0,0 @@
-# python -m venv env   --- TO CREATE A VENV
-# .\myenv\Scripts\Activate.ps1    --- TO ACTICATE IT
-# pip freeze           --- TO GET PACKAGES NAMES
-# pip freeze > REQUIREMENTS.TXT          --- TO LIST THOSE PACKAGES NAMES IN TXT FILE
-# pip install -r install requirements.txt  --- TO INSTALL PACKAGES LISTED IN TXT FILE
